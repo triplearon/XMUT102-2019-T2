67e05a8aa0e891a93ec7bef10b7f02279133d1a7
triplearon <55239194+triplearon@users.noreply.github.com>
2019-11-16 19:59:32 +0800

    Update BalloonGame.java

--- XMUT102-homework/Assig7/BalloonGame/BalloonGame.java
+++ XMUT102-homework/Assig7/BalloonGame/BalloonGame.java
@@ -1,68 +1,8 @@
-// This program is copyright VUW.
-// You are granted permission to use it to construct your answer to a XMUT102 assignment.
-// You may not distribute it in any other way without permission.
-
-/* Code for XMUT102 - 2019T2, Assignment 7
- * Name:
- * Username:
- * ID:
- */
 
 import ecs100.*;
 import java.util.*;
 import java.awt.Color;
 
-/** Program for a simple game in which the player has to blow up balloons
- *   on the screen.
- *  The game starts with a collection of randomly placed small balloons
- *    (coloured circles) on the graphics pane.
- *  The player then clicks on balloons to blow them up by a small amount
- *   (randomly increases the radius between 2 and 6 pixels).
- *  If an expanded balloon touches another balloon, then they both "burst" and go grey.
- *  The goal is to get the largest score. The score is the total of the
- *   sizes (areas) of all the active balloons, minus the total size of all
- *   the burst balloons.
- *  At each step, the current score is recalculated and displayed,
- *   along with the highest score that the player has achieved so far.
- *  At any time, the player may choose to stop and "lock in" their score.
- *
- *  The BalloonGame class has a field containing an Arraylist of Balloon objects
- *   to represent the current set of Balloons on the screen.
- *  It has a field to hold the highest score.
- *
- *  The New Game button should start a new game.
- *  The Lock Score button should finish the current game, updating the highest score
- *
- *  Clicking (ie, releasing) the mouse on the graphics pane is the main "action"
- *  of the game. The action should do the following
- *    Find out if the mouse was clicked on top of any balloon.
- *    If so,
- *      Make the balloon a bit larger and redraw it.
- *      Check whether the balloon is touching any other balloon.
- *      If so
- *         burst the two balloons (which will make them go grey)
- *         redraw the burst Balloons
- *      Recalculate and redisplay the score
- *   If all the balloons are gone, the game is over.
- *    
- *   To start a game, the program should
- *       Clear the graphics pane
- *       Initialise the score information
- *       Make a new list of Balloons at random positions
- *       Print a message 
- *
- *   If the game is over, the program should
- *      Update the highest score if the current score is better,
- *      Print a message reporting the scores,
- *      Start a new game.
- *     
- *   There are lots of ways of designing the program. It is not a good idea
- *   to try to put everything into one big method.
- *        
- *  Note that the Balloon class is written for you. Make sure that you know
- *   all its methods - no marks for redoing code that is given to you.
- *    
- */
 public class BalloonGame {
     private static final int MAX_BALLOONS = 20;
 
@@ -78,8 +18,6 @@ public class BalloonGame {
     private int balloonsAmount=0;
     private int pushTimes;
     private int ApushTimes;
-    
-    
     
     private ArrayList<Double> pX=new ArrayList<>();
     private ArrayList<Double> pY=new ArrayList<>();
@@ -153,11 +91,6 @@ public class BalloonGame {
       }
     }
 
-    /** Start the game:
-     *  Clear the graphics pane
-     *  Initialise the score information 
-     *  Make a new set of Balloons at random positions
-     */
     public void restartGame(){
         UI.clearGraphics();
         pX.clear();
@@ -167,10 +100,7 @@ public class BalloonGame {
         highScore=0;
         isScoreLock=false;
         pushTimes=3;
-        
-        
-        
-        
+      
         this.setBalloon();
         for(int i=0;i<pX.size();i++){
             balloons.add(new Balloon(pX.get(i),pY.get(i)));
@@ -217,15 +147,7 @@ public class BalloonGame {
         return Math.hypot(x1-x2,y1-y2);
     }
 
-    /**
-     * Main game action:
-     *  Find the balloon at (x,y) if any,
-     *  Expand it 
-     *  Check whether it is touching another balloon,
-     *  If so, update the penalty, burst both balloons.
-     *  Report the score.
-     *  If there are no active balloons left, end the game.
-     */
+ 
     public void  doMouse(String action, double x, double y){
         if(action.equals("pressed")){
             ball=this.findBalloonOn(x,y);
